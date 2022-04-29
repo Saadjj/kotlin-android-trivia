fcf87e712403210c50ff21c21e17926781a4b963
ilev <ilev.andrei@yandex.ru>
2022-04-29 09:40:05 +0300

    ok step 23

--- app/src/main/java/com/example/android/navigation/GameWonFragment.kt
+++ app/src/main/java/com/example/android/navigation/GameWonFragment.kt
@@ -20,6 +20,7 @@ import android.content.Intent
 import android.os.Bundle
 import android.view.*
 import android.widget.Toast
+import androidx.core.app.ShareCompat
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.navigation.findNavController
@@ -36,9 +37,7 @@ class GameWonFragment : Fragment() {
             view.findNavController().navigate(GameWonFragmentDirections.actionGameWonFragmentToGameFragment())
         }
 
-        Toast.makeText(context,
-            "NumCorrect: ${args.numCorrect}, NumQuestions: ${args.numQuestions}",
-            Toast.LENGTH_LONG).show()
+
         setHasOptionsMenu(true)
         return binding.root
     }
@@ -46,16 +45,18 @@ class GameWonFragment : Fragment() {
     override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
         super.onCreateOptionsMenu(menu, inflater)
         inflater?.inflate(R.menu.winner_menu, menu)
+        if(null==getShareIntent().resolveActivity(activity!!.packageManager)){
+            menu.findItem(R.id.share)?.setVisible(false)
+        }
     }
 
-    private fun getShareIntent(): Intent {
-        val args = GameWonFragmentArgs.fromBundle(arguments!!)
-        val shareIntent= Intent(Intent.ACTION_SEND)
-        shareIntent.setType("text/plain")
-            .putExtra(Intent.EXTRA_TEXT,
-        getString(R.string.share_success_text, args.numCorrect,
-        args.numQuestions))
-        return shareIntent
+    // Creating our Share Intent
+    private fun getShareIntent() : Intent {
+        val args = GameWonFragmentArgs.fromBundle(requireArguments())
+        return ShareCompat.IntentBuilder.from(activity!!)
+            .setText(getString(R.string.share_success_text, args.numCorrect, args.numQuestions))
+            .setType("text/plain")
+            .intent
     }
 
     private fun shareSucess(){
